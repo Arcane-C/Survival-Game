8ab820e4f59e5fd09cff51ed8ff98ad3059efeab
Arcane-C <fuericitasu@gmail.com>
2025-05-13 11:30:10 +0700

    error sesi 11
    menit ke 50 error pada bagian script attack line ke-12 dan ke-30

--- Assets/Scripts/Attack.cs
+++ Assets/Scripts/Attack.cs
@@ -1,6 +1,32 @@
+using System.Collections;
 using UnityEngine;
+//using UnityEngine.Pool;
 
 public class Attack : MonoBehaviour
 {
-    
+    [SerializeField] private float attackDelay;
+    private ObjectPool pool;
+
+    private void Awake()
+    {
+        pool = FindAnyObjectByType<ObjectPool>();
+    }
+    private void Start()
+    {
+        StartCoroutine(AttackEnumerator());
+    }
+
+    private IEnumerator AttackEnumerator()
+    {
+        while(true)
+        {
+            Shoot();
+            yield return new WaitForSeconds(attackDelay);
+        }
+    }
+
+    private void Shoot()
+    {
+        pool.Request<Bullet>();
+    }
 }
